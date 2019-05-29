ce28537c3a35fa7acb018389ff8c6a78c237ddb3
Hwarang Choi <az8606@naver.com>
2019-05-29 11:47:46 +0900

    Update index.js

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,9 +1,10 @@
 import Router from 'koa-router';
-import { Register, Login  } from './auth.controller';
+import { Register, Login, showInfo } from './auth.controller';
 
 const auth = new Router();
 
 auth.post('/register', Register);
 auth.post('/login', Login);
+auth.get('/showUser', showInfo);
 
 export default auth;
